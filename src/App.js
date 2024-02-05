aad514f8b98a6f13740426e87957c17671d9addd
iamyeongg <syz101@naver.com>
2024-02-06 01:24:33 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Layout from './components/layout';
+
+function App() {
+  return (
+    <Layout>
+      <div className="bg-red-500"> Tailwind Css 적용 테스트 </div>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../index.css";
+
+export default function Footer() {
+  return (
+    <div className="bg-red">
+      <h2>header</h2>
+    </div>
+  );
+}
