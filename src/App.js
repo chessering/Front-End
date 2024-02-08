272c0c09f2e45031b4bafca6e1761ff02f59dee2
sj <yi9353@khu.ac.kr>
2024-02-08 23:33:51 +0900

    마이페이지 css 기본 틀

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
+import Myinfo from "./pages/Myinfo.jsx";
 import Layout from './components/layout';
 
 function App() {
   return (
     <Layout>
       <div className="bg-red-500"> Tailwind Css 적용 테스트 </div>
+      <Myinfo></Myinfo>
     </Layout>
   );
 }
