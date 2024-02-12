f5ed12a4316eac360848557e125f891d5839dceb
kimjunseo <juny7s6@khu.ac.kr>
2024-02-12 22:14:59 +0900

    Feat: 업로드 기능 구현

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,55 +1,169 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
+import { useForm } from "react-hook-form"
+import axios from 'axios'
 import { ReactComponent as UploadIcon } from "../../assets/images/icon_Image Upload.svg"
+
 export default function Upload() {
+  const imageInput = useRef(null);
+  const [imgURL, setImgURL] = useState(null);
+  const [isDrag, setIsDrag] = useState(false);
+  
+  const handleChangeImg = (e) => {
+    const file = e.target.files[0];
+    readImg(file)
+  }
+
+  const handleDragStart = () => {
+    setIsDrag(true)
+  }
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setIsDrag(true)
+  }
+  const handleDragEnd = () => {
+    setIsDrag(false)
+  }
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if(file.type.startsWith('image'))
+      readImg(file)
+    else{
+      setError('img_url',{
+        message: "이미지 파일을 첨부해주세요."
+      },{
+        shouldFocus: true
+      })
+    }
+  }
+
+  const readImg = async (file) => {    
+    try {
+      setValue('img_url', URL.createObjectURL(file));
+      clearErrors('img_url');
+    } catch (error) {
+      setError('img_url',{
+        message: "사진을 첨부해주세요."
+      },{
+        shouldFocus: true
+      })
+    }
+  }
+
+  useEffect(() => {
+    setValue('img_url', null);
+    setError('img_url',{
+      message: "사진을 첨부해주세요."
+    },{
+      shouldFocus: true
+    })
+  }, [])
+
+
+  const {
+    register,
+    setValue,
+    setError,
+    clearErrors,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm({mode:"onChange"});
+
+  const onSubmit = (data) => {
+    console.log(data);
+    alert('제출됨')
+    // axios
+    //   .post(`${""}/api/upload`, {
+    //     img_url: data.img_url,
+    //     category: data.category,
+    //     title: data.title,
+    //     explanation: data.description,
+    //     link: data.link,
+    //     tag: data.tag
+    //   })
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err))
+  }
+
   return (
     <UploadWrap>
       <Container>
         <Title>업로드</Title>
-        <UploadForm>
-          <ImageUpload>
-            <LeftContainer>
-              <ImageLabel>
-              <ImageInput type='file'></ImageInput>
-              <UploadIcon></UploadIcon>
-              <Drop>이미지를 끌어다 놓기</Drop>
-              <Notices>
-                <Notice>ㆍ 권리를 소유한 이미지만 업로드</Notice>
-                <Notice>ㆍ 다른 사람들의 지적 재산권 존중</Notice>
-                <Notice>ㆍ 이미지는 선명한 원본임</Notice>
-              </Notices>
-              </ImageLabel>
-            </LeftContainer>
-            <UploadBtn onClick={(e) => {
+        <UploadForm onSubmit={handleSubmit(onSubmit)}>
+          <LeftContainer>
+            <ImageLabel ref={imageInput} isdrag={isDrag}
+              onDragEnter={handleDragStart}
+              onDragOver={handleDragOver}
+              onDragLeave={handleDragEnd}
+              onDrop={handleDrop}>
+              <ImageInput id='img_url' type='file' accept='image/*'  onChange={handleChangeImg}/>
+              {watch('img_url') ? (<Preview src={watch('img_url')} alt='upload'></Preview>) : 
+              (<GuideWrap>
+                <GuideContainer>
+                  <UploadIcon></UploadIcon>
+                  <Drop>이미지를 끌어다 놓기</Drop>
+                  <Notices>
+                    <Notice>ㆍ 권리를 소유한 이미지만 업로드</Notice>
+                    <Notice>ㆍ 다른 사람들의 지적 재산권 존중</Notice>
+                    <Notice>ㆍ 이미지는 선명한 원본임</Notice>
+                  </Notices>
+                </GuideContainer>
+              </GuideWrap>) 
+              }
+            </ImageLabel>
+            <Browse onClick={(e) => {
               e.preventDefault();
-            }}>찾아보기</UploadBtn>
-          </ImageUpload>
+              imageInput.current.click();
+            }}>찾아보기</Browse>
+          </LeftContainer>
           <RightContainer>
             <Others>
               <Label>
                 카테고리
-                <Input/>
+                <Input id='category' 
+                  {...register('category',{
+                    required: true,
+                  })}/>
               </Label>
               <Label>
                 제목
-                <Input/>
+                <Input id='title' 
+                  {...register('title', {
+                    required: true,
+                  })}/>
               </Label>
               <Label>
                 설명
-                <Input/>
+                <Input id='description' 
+                  {...register('description', {
+                    required: true,
+                  })}/>
               </Label>
               <Label>
                 관련링크
-                <Input/>
+                <Input id='link' 
+                  {...register('link', {
+                    required: true,
+                  })}/>
               </Label>
               <Label>
                 태그 n개
-                <Input/>
+                <Input id='tags' 
+                  {...register('tag', {
+                    required: true,
+                  })
+                }/>
               </Label>
             </Others>
-            <Submit type='submit' onClick={(e) => {
-              e.preventDefault();
-            }}></Submit>
+            <Submit type='submit'>제출</Submit>
+            {errors.img_url && <div>{errors.img_url.message}</div>}
+            {errors.category && <div>cat에러</div>}
+            {errors.title && <div>title에러</div>}
+            {errors.description && <div>desc에러</div>}
+            {errors.link && <div>link에러</div>}
+            {errors.tag && <div>tag에러</div>}
           </RightContainer>
         </UploadForm>
       </Container>
@@ -95,7 +209,7 @@ const UploadForm = styled.form`
   justify-content: space-between;
 `
 
-const ImageUpload = styled.div`
+const LeftContainer = styled.div`
   height: 442px;
 
   display: flex;
@@ -104,16 +218,44 @@ const ImageUpload = styled.div`
   align-items: center;
 `
 
-const LeftContainer = styled.label`
+const ImageLabel = styled.label`
   width: 588px;
   height: 364px;
-  
+  z-index: 10;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  border: ${(props) => props.isdrag ? '1px solid red' : '1px solid blue'};
+  cursor: pointer;
+`
+
+const GuideWrap = styled.div`
+  width: 588px;
+  height: 364px;
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
   background: #D9D9D9;
+`
+
+const GuideContainer = styled.div`
+  height: 161px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Preview = styled.img`
+  width: 588px;
+  height: 364px;
 `
 
 const RightContainer = styled.div`
@@ -125,14 +267,6 @@ const RightContainer = styled.div`
   justify-content: space-between;
 `
 
-const ImageLabel = styled.div`
-  height: 161px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-`
 const Drop = styled.div`
   width: 215px
   height: 29px
@@ -171,7 +305,7 @@ const ImageInput = styled.input`
   display: none;
 `
 
-const UploadBtn = styled.button`
+const Browse = styled.button`
   width: 141px;
   height: 42px;
 
@@ -218,12 +352,13 @@ const Label = styled.label`
 const Input = styled.input`
   width: 486px;
   height: 49px;
+  padding-left: 10px;
 
   background: #F2F1F1;
   border-radius: 7px;
 `
 
-const Submit = styled.input`
+const Submit = styled.button`
   width: 93px;
   height: 42px;
   
@@ -233,5 +368,6 @@ const Submit = styled.input`
   border-radius: 7px;
 
   color: #FFFFFF;
+  cursor: pointer;
 
 `
